50037717ca760a4049940a6b9a07af9b551f8b51
salihedneergit <salihindiacp@gmail.com>
2024-11-14 08:55:44 +0530

    final

---
+++ MM/string_reverse.asm
@@ -0,0 +1,47 @@
+data SEGMENT
+    MSG1 DB 10,13, 'ENTER THE STRING:$’ 
+    MSG2 DB 10,13, ’REVERSED STRING:$’
+    STR1 DB 50 DUP(0)
+data ENDS
+code SEGMENT
+    ASSUME CS:code, DS:data
+START:
+    MOV AX, DATA
+    MOV DS, AX 
+    LEA DX, MSG1
+    MOV AH, 09H 
+    INT 21H
+    LEA SI, STR1 
+    LEA DI, STR1 
+    MOV AH, 01H
+NEXT:
+    INT 21H
+    CMP AL, ODH 
+    JE TERMINATE 
+    MOV [DI], AL 
+    INC DI
+    JMP NEXT
+TERMINATE:
+    MOV BYTE PTR[DI],’$’
+    DEC DI
+REVERSE:
+    CMP SI, DI
+    JAE PRINT 
+    MOV AL, [SI] 
+    MOV BL, [DI] 
+    MOV [DI], AL 
+    MOV [SI], BL 
+    INC SI
+    DEC DI
+JMP REVERSE
+PRINT:
+    LEA DX, MSG2
+    MOV AH, 09H 
+    INT 21H
+    LEA DX, STR1
+    MOV AH, 09H 
+    INT 21H
+    MOV AH, 4CH
+    INT 21H 
+code ENDS
+END START
